14874cb303209e73c2741a81abae4300d9f55bc2
luna <paulkingdev7@gmail.com>
2025-01-11 21:21:10 +0300

    Update README and initial.lua

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # write-lua
+
+Lua is elegant!
+- lua uses "mechanisms over policies"
+- lua is designed to be embedded
+- lua is cool!
+
+
+# comments
+-- This is a comment. It starts with two dashes
---
+++ initial.lua
@@ -0,0 +1,18 @@
+-- This is a comment
+--[[ This is also 
+    a comment that spans more than one line
+
+    it spans many line!
+--]]
+ 
+local number  = 5
+local singleString = "hello world"
+local crazy = [[comment
+    can span more than one line
+]]
+
+local function hello()
+  print("Hello World! This is lua")
+end
+
+hello()
